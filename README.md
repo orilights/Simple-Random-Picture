e043003a095e5bf532410a45b78d8243167bc005
Amatls <61224409+amatlsmisr@users.noreply.github.com>
2021-02-17 15:33:49 +0800

    0.1

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # Simple-Random-Picture
-简易的随机图片API
+一个简易的随机图片API
+
+## 参数
+
+| 参数 | 可选/必选 | 类型   | 备注                                      |
+| ---- | --------- | ------ | ----------------------------------------- |
+| id   | 可选      | number | 如ID对应图片不存在则随机返回一张图片      |
+| type | 可选      | string | [json]以json返回   [quantity]返回图片总数 |
+
+
+
+## 使用
+
+> api.php?id=[图片ID]&type=[返回类型]
+
+### 直接使用
+
+随机跳转至一张图片
+
+### 带参数使用
+
+示例：api.php?id=6
+
+```
+GET ./api.php?id=6
+
+HTTP/1.1 302 Found
+Location:https://p4.amarea.cn/i/8oz6emqw.jpg
+```
+
+示例：api.php?id=6&type=json
+
+```
+GET ./api.php?id=6&type=json
+
+HTTP/1.1 200 OK
+Content-Type: text/json
+{"code":200,"id":6,"url":"https:\/\/p4.amarea.cn\/i\/8oz6emqw.jpg"}
+```
+
+示例：api.php?type=quantity
+
+```
+GET ./api.php?type=quantity
+
+HTTP/1.1 200 OK
+7
+```
+
+## 测试地址
+
+[api.amarea.cn/getemilia](https://api.amarea.cn/getemilia)
+
+[api.amarea.cn/getemilia?id=6&type=json](https://api.amarea.cn/getemilia?id=6&type=json)
+
---
+++ api.php
@@ -0,0 +1,47 @@
+<?php
+
+const NAME = 'emilia';
+
+//读取数据
+$data = file('data/' . NAME . '.csv', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+if (empty($data[0])){
+    header('HTTP/1.1 503 Service Unavailable');
+    die ('503 Service Unavailable');
+}
+
+//初始化
+header('Access-Control-Max-Age: 86400');
+header('Access-Control-Allow-Origin: *');
+$id = $_REQUEST['id'];
+$type = $_REQUEST['type'];
+$quantity = count($data);
+
+//处理id
+if(!isset($id)){
+    $id = array_rand($data) + 1;
+}
+settype($id,'integer');
+if ($id <= 0 || $id > $quantity){
+    $id = array_rand($data) + 1;
+}
+
+//输出或跳转
+$pic = $data[$id - 1];
+switch ($type) {
+    case 'quantity':
+        echo $quantity;
+        break;
+    case 'json':
+        $result = [
+            'code' => 200,
+            'id' => $id,
+            'url' => $pic
+        ];
+        header('Content-Type: text/json');
+        echo json_encode($result);
+        break;
+    default:
+        header("Location:" . $pic);
+}
+
+?>
